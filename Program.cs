84950860a2cafa15dd02707b4d9e4b5dd0dcff72
teesloane <tylersloane@gmail.com>
2019-06-27 10:43:35 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace circleci_demo_windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
